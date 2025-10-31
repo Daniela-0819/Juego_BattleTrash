4b740295b6885f95a29dff0addad82eb0b79e9cd
Daniela-0819 <danicaslopera2005@gmail.com>
2025-10-31 12:52:58 -0500

    finall

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 // En backend/controllers/authController.js
 const User = require('../models/User');  // ../ significa "subir un nivel"
